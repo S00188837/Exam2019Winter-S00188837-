66a5a6bc5874f3801e04dbcdd15b24adf441ad8d
Domas Ruginis <S00188837@mail.itsligo.ie>
2019-12-11 09:50:23 +0000

    Add project files.

---
+++ Exam2019Winter(S00188837)/Players.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam2019Winter_S00188837_
+{
+    class Players
+    {
+
+        public enum PlayerType
+        {
+            Goalkeeper,
+            Defender,
+            Midfielder,
+            Forward
+        }
+
+        public static Players[] players = new Players[]
+        {
+
+
+
+
+        };
+
+        //properties
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public PlayerType playerType { get; set; }
+        public DateTime PlayerDate { get; set; }
+        public int Age { get; set; }
+
+
+        
+
+    }
+}
